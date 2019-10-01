98dd5e562bb7323b22c164b7c10093069df7b1d9
Samujjwal <cs16resch01001@iith.ac.in>
2019-10-01 12:10:17 +0530

    Modified data preload code.

--- data_loaders/Prepare_Data.py
+++ data_loaders/Prepare_Data.py
@@ -85,12 +85,23 @@ class Prepare_Data:
             batch_y.append(Y[k])
         return np.stack(batch_x),np.stack(batch_y)
 
-    def get_features(self,return_cat_indices=False,multi_label=False):
-        """ Generates feature vectors of input documents. """
-        keys = list(self.classes_selected.keys())
-        train_sentences,train_classes = self.create_batch_repeat(self.sentences_selected,self.classes_selected,keys)
-        features = self.txt2vec(train_sentences)
-        categories_hot = self.mlb.transform(train_classes)
+    def get_input_batch(self,txts:dict,sample2cats:dict,keys:list=None,return_cat_indices: bool = False,multi_label: bool = True) ->\
+            [np.ndarray,np.ndarray,np.ndarray]:
+        """Generates feature vectors of input documents.
+
+        :param txts:
+        :param sample2cats:
+        :param keys:
+        :param return_cat_indices:
+        :param multi_label:
+        :return:
+        """
+        if keys is None:
+            sample_ids = list(txts.keys())
+            batch_size = int(0.7 * len(sample_ids))
+            _,keys = File_Util.get_batch_keys(sample_ids,batch_size=batch_size, remove_keys=False)
+        txt_vecs_keys,sample2cats_keys = self.create_batch_repeat(txts,sample2cats,keys)
+        sample2cats_keys_hot = self.mlb.transform(sample2cats_keys)
 
         if return_cat_indices:
             if multi_label:
@@ -152,173 +163,27 @@ class Prepare_Data:
                                   filepath=join(self.dataset_dir,self.dataset_name))
         return txts2vec_map,cats2vec_map
 
-        ## MultiLabelBinarizer only takes list of lists as input. Need to convert our list of ints to list of lists.
+    def load_raw_data(self,load_type: str = 'all', return_values=True):
+        """ Loads the json data provided by param "load_type".
+
+        :param return_values:
+        :param load_type: Which data to load: Options: ['train', 'val', 'test']
+        """
+        self.txts_sel,self.sample2cats_sel,self.cats_sel,self.cats_all = self.dataset_loader.get_data(load_type=load_type)
+        self.remain_sample_ids = list(self.txts_sel.keys())
+        self.cat2sample_map = self.invert_cat2samples(self.sample2cats_sel)
+        self.remain_cat_ids = list(self.cats_sel.keys())
+
+        ## MultiLabelBinarizer only take list of list as input. Need to convert "list of int" to "list of list".
         cat_ids = []
         for cat_id in self.cats_all.values():
             cat_ids.append([cat_id])
         self.mlb.fit(cat_ids)
 
-        # self.idf_dict = self.clean.calculate_idf(docs=self.sentences_selected.values())
-        if return_loaded:
-            return self.sentences_selected,self.classes_selected,self.categories_selected,self.categories_all
+        self.idf_dict = self.txt_process.calculate_idf(docs=self.txts_sel.values())
 
-    def txt2vec(self,sentences: list,vectorizer=config["prep_vecs"]["vectorizer"],
-                tfidf_avg=config["prep_vecs"]["tfidf_avg"]):
-        """
-        Creates vectors from input_texts based on [vectorizer].
-
-        :param max_vec_len: Maximum vector length of each document.
-        :param num_chunks: Number of chunks the input_texts are to be divided. (Applicable only when vectorizer = "chunked")
-        :param input_size: Embedding dimension for each word.
-        :param sentences:
-        :param vectorizer: Decides how to create the vector.
-            "chunked" : Partitions the whole text into equal length chunks and concatenates the avg of each chunk.
-                        vec_len = num_chunks * input_size
-                        [["these", "are"], ["chunks", "."]]
-
-            "sentences" : Same as chunked except each sentence forms a chunk. "\n" is sentence separator.
-                        vec_len = max(num_sents, max_len) * input_size
-                        ["this", "is", "a", "sentence", "."]
-                        NOTE: This will create variable length vectors.
-
-            "concat" : Concatenates the vectors of each word, adds padding to make equal length.
-                       vec_len = max(num_words) * input_size
-
-            "word_avg" : Take the average of vectors of all the words.
-                       vec_len = input_size
-                       [["these"], ["are"], ["words"], ["."]]
-
-            "doc2vec" : Use Gensim Doc2Vec to generate vectors.
-                        https://radimrehurek.com/gensim/models/doc2vec.html
-                        vec_len = input_size
-                        ["this", "is", "a", "document", "."]
-
-        :param tfidf_avg: If tf-idf weighted avg is to be taken or simple.
-            True  : Take average based on each words tf-idf value.
-            False : Take simple average.
-
-        :returns: Vector length, numpy.ndarray(batch_size, vec_len)
-        """
-        # sentences = util.clean_sentences(sentences, specials="""_-@*#'"/\\""", replace='')
-        if vectorizer == "doc2vec":
-            if self.doc2vec_model is None:
-                self.doc2vec_model = self.text_encoder.load_doc2vec(sentences)
-            vectors_dict = self.text_encoder.get_doc2vecs(sentences,self.doc2vec_model)
-            return vectors_dict
-        elif vectorizer == "word2vec":
-            w2v_model = self.text_encoder.load_word2vec()
-            sentences = list(filter(None,sentences))  ## Removing empty items.
-            return self.create_doc_vecs(sentences,w2v_model=w2v_model)
-        else:
-            raise Exception("Unknown vectorizer: [{}]. \n"
-                            "Available options: ['doc2vec','word2vec']"
-                            .format(vectorizer))
-
-    oov_words_dict = {}
-
-    def create_doc_vecs(self,sentences: list,w2v_model,use_idf=config["prep_vecs"]["idf"],concat_axis=0,
-                        input_size=config["prep_vecs"]["input_size"]):
-        """
-        Calculates the average of vectors of all words within a chunk and concatenates the chunks.
-
-        :param use_idf: Flag to decide if idf is to be used.
-        :param input_size:
-        :param concat_axis: The axis the vectors should be concatenated.
-        :param sents_chunk_mode:
-        :param w2v_model:
-        :param sentences: Dict of texts.
-        :returns: Average of vectors of chunks. Dim: input_size.
-        """
-        docs_vecs = []  # List to hold generated vectors.
-        for i,doc in enumerate(sentences):
-            chunks = self.partition_doc(doc)
-            chunks = list(filter(None,chunks))  ## Removing empty items.
-            vecs = self.sum_word_vecs(chunks,w2v_model)
-            docs_vecs.append(vecs)
-        return np.stack(docs_vecs)
-
-    def sum_word_vecs(self,words: list,w2v_model,input_size=config["prep_vecs"]["input_size"],avg=True):
-        """ Generates a vector of [input_size] using the [words] and [w2v_model].
-
-        :param avg: If average to be calculated.
-        :param words: List of words
-        :param w2v_model: Word2Vec model by Gensim
-        :param input_size: Dimension of each vector
-        :return: Vector of sum of all the words.
-        """
-        # oov_words_dict = {}  ## To hold out-of-vocab words.
-        sum_vec = None
-        for i,word in enumerate(words):
-            ## Multiply idf of that word with the vector
-            try:  ## If word exists in idf dict
-                idf = self.idf_dict[word]
-            except KeyError as e:  ## If word does not exists in idf_dict, multiply 1
-                # logger.info("[{}] not found in idf_dict.".format(word))
-                idf = 1
-            if word in w2v_model.vocab:  ## If word is present in model
-                if sum_vec is None:
-                    sum_vec = w2v_model[word] * idf
-                else:
-                    sum_vec = np.add(sum_vec,w2v_model[word] * idf)
-            elif word in Prepare_Data.oov_words_dict:  ## If word is OOV
-                if sum_vec is None:
-                    sum_vec = Prepare_Data.oov_words_dict[word] * idf
-                else:
-                    sum_vec = np.add(sum_vec,Prepare_Data.oov_words_dict[word] * idf)
-            else:  ## New unknown word, need to create random vector.
-                new_oov_vec = np.random.uniform(-0.5,0.5,input_size)
-                # w2v_model.add(word, new_oov_vec)  ## For some reason, gensim word2vec.add() not working.
-                Prepare_Data.oov_words_dict[word] = new_oov_vec
-                if sum_vec is None:
-                    sum_vec = Prepare_Data.oov_words_dict[word] * idf
-                else:
-                    sum_vec = np.add(sum_vec,Prepare_Data.oov_words_dict[word] * idf)
-        if avg:
-            sum_vec = np.divide(sum_vec,float(len(words)))
-
-        return np.stack(sum_vec)
-
-    def partition_doc(self,sentence: str,sents_chunk_mode: str = config["text_process"]["sents_chunk_mode"],
-                      num_chunks: int = config["prep_vecs"]["num_chunks"]) -> list:
-        """
-        Divides a document into chunks based on the vectorizer.
-
-        :param num_chunks:
-        :param sentence:
-        :param sents_chunk_mode:
-        :param doc_len:
-        :return:
-        """
-        chunks = []
-        if sents_chunk_mode == "concat":
-            words = self.txt_process.tokenizer_spacy(sentence)
-            for word in words:
-                chunks.append(word)
-        elif sents_chunk_mode == "word_avg":
-            chunks = self.txt_process.tokenizer_spacy(sentence)
-        elif sents_chunk_mode == "sentences":
-            chunks = self.txt_process.sents_split(sentence)
-        elif sents_chunk_mode == "chunked":
-            splitted_doc = self.txt_process.tokenizer_spacy(sentence)
-            doc_len = len(splitted_doc)
-            chunk_size = doc_len // num_chunks  ## Calculates how large each chunk should be.
-            index_start = 0
-            for i in range(num_chunks):
-                batch_portion = doc_len / (chunk_size * (i + 1))
-                if batch_portion > 1.0:
-                    index_end = index_start + chunk_size
-                else:  ## Available data is less than chunk_size
-                    index_end = index_start + (doc_len - index_start)
-                logger.info('Making chunk of tokens from [{0}] to [{1}]'.format(index_start,index_end))
-                chunk = splitted_doc[index_start:index_end]
-                chunks.append(chunk)
-                index_start = index_end
-        else:
-            raise Exception("Unknown document partition mode: [{}]. \n"
-                            "Available options: ['concat','word_avg (Default)','sentences','chunked']"
-                            .format(sents_chunk_mode))
-        chunks = list(filter(None,chunks))  ## Removes empty items, like: ""
-        return chunks
+        if return_values:
+            return self.txts_sel,self.sample2cats_sel,self.cats_sel,self.cats_all
 
     def normalize_inputs(self):
         """
