7902cbd6b3ebc7806655080979e8c52caa4a16e0
Samujjwal <cs16resch01001@iith.ac.in>
2019-11-15 15:09:30 +0530

    Copied function to visualize gradient flow (not tested).

--- main_XCGCN.py
+++ main_XCGCN.py
@@ -360,6 +360,62 @@ def train(epoch: int,model,optimizer,features: torch.Tensor,adj: torch.Tensor,
            (time.time() - t)
 
 
+def plot_grad_flow(named_parameters):
+    """ Plug this API after the loss.backward() during the training as follows -
+
+    loss = self.criterion(outputs, labels)
+    loss.backward()
+    plot_grad_flow(model.named_parameters())
+
+    https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/7
+    """
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+
+
+def plot_grad_flow(named_parameters):
+    """Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow"""
+    ave_grads = []
+    max_grads= []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    # plt.legend([Line2D([0], [0], color="c", lw=4),
+    #             Line2D([0], [0], color="b", lw=4),
+    #             Line2D([0], [0], color="k", lw=4)],
+    #             ['max-gradient', 'mean-gradient', 'zero-gradient'])
+
+
+
 def train_emb(epoch: int,model,optimizer,features: torch.Tensor,
               adj: torch.Tensor,label_emb: torch.Tensor,labels: torch.Tensor,
               idx_train: torch.Tensor,idx_val: torch.Tensor) -> tuple:
@@ -479,7 +535,7 @@ def main(args):
     _,label_embs = create_lbl_embs(samples2cats_map,cats2vec_map)
 
     # label_embs = torch.FloatTensor(label_embs)
-    label_embs = Variable(torch.from_numpy(label_embs),requires_grad=False).float()
+    label_embs = Variable(torch.from_numpy(label_embs),requires_grad=True).float()
 
     # Train model
     train_losses,train_accs,val_losses,val_accs,train_times = [],[],[],[],[]
